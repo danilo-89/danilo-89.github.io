f0fc7cfa4a83d8968ab9a3e7ec955c6d8aec3ede
danilo-89 <danilo89abcd@gmail.com>
2021-04-12 23:17:08 +0200

    new changes

--- script/main.js
+++ script/main.js
@@ -1,24 +1,20 @@
-function myFunction() {
-    var element = document.getElementById("myDIV");
-    element.classList.toggle("mystyle");
-}
-
-console.log("!ok");
+const navToggleBtn = document.getElementById("navToggleBtn");
+const navbarMain = document.getElementById("navbarMain");
 
 
-document.getElementById("navToggleBtn").addEventListener("click", function() {
-    console.log("click");
-    document.getElementById("navbarMain").classList.toggle("active");
+navToggleBtn.addEventListener("click", function() {
+    navbarMain.classList.toggle("active");
     this.classList.toggle("click");
-  });
+});
 
-
-
-  document.getElementById("navbarMain").addEventListener("click", function(event) {
-    
+// close navigation when clicked outside nav ul
+navbarMain.addEventListener("click", function(event) {
     if (event.target===event.currentTarget) {
-        console.log("click");
-        document.getElementById("navbarMain").classList.remove("active");
-        document.getElementById("navToggleBtn").classList.remove("click");
+        hideNav();
     }
-  });
+});
+
+function hideNav() {
+    navbarMain.classList.remove("active");
+    navToggleBtn.classList.remove("click");
+}
