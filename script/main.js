701113f0120d03b93ce6c15c666a207991927999
danilo-89 <danilo89abcd@gmail.com>
2021-04-12 23:04:44 +0200

    new changes

--- script/main.js
+++ script/main.js
@@ -9,4 +9,16 @@ console.log("!ok");
 document.getElementById("navToggleBtn").addEventListener("click", function() {
     console.log("click");
     document.getElementById("navbarMain").classList.toggle("active");
+    this.classList.toggle("click");
+  });
+
+
+
+  document.getElementById("navbarMain").addEventListener("click", function(event) {
+    
+    if (event.target===event.currentTarget) {
+        console.log("click");
+        document.getElementById("navbarMain").classList.remove("active");
+        document.getElementById("navToggleBtn").classList.remove("click");
+    }
   });
